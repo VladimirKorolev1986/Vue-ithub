f6cddf296087f847d4ef8907a23ed625d4059c51
VladimirKorolev <theing1986@gmail.com>
2025-03-19 22:26:27 +0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <script setup></script>
 
 <template>
-  <h1>Hello world!</h1>
+  <div class="bg-gray-100 w-full max-w-[1440px] mx-auto">
+    <header class="h-16 flex">
+      <div>
+        <img src="/Logo.svg" alt="Лого" />
+      </div>
+      <div>
+        <img src="/Search.svg" alt="Поиск" />
+        <p>search</p>
+        <input type="text" />
+      </div>
+    </header>
+
+    <main class="container mx-auto px-4 md:px-10 lg:px-20 py-10">
+      <h1 class="text-2xl md:text-4xl font-bold">Привет, мир!</h1>
+    </main>
+
+    <footer class="bg-gray-800 text-white py-4 text-center">Footer</footer>
+  </div>
 </template>
