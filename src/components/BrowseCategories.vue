e3cddb1938cb5eeddc39c9884b11072302432c41
VladimirKorolev <theing1986@gmail.com>
2025-03-23 18:52:40 +0300

    finish categories

--- src/components/BrowseCategories.vue
+++ src/components/BrowseCategories.vue
@@ -5,13 +5,26 @@
       <img src="/Icon/Arrows.svg" alt="Стрелки" class="arrows" />
     </div>
     <div class="category-items">
-      <CategoryItem />
+      <CategoryItem
+        v-for="(cat, index) in categories"
+        :key="index"
+        :icon="cat.icon"
+        :name="cat.name"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
 import CategoryItem from './CategoryItem.vue'
+const categories = [
+  { icon: '/Icon/Phones.svg', name: 'Phones' },
+  { icon: '/Icon/SmartWatches.svg', name: 'Smart Watches' },
+  { icon: '/Icon/Cameras.svg', name: 'Cameras' },
+  { icon: '/Icon/Headphones.svg', name: 'Headphones' },
+  { icon: '/Icon/Computers.svg', name: 'Computers' },
+  { icon: '/Icon/Gaming.svg', name: 'Gaming' },
+]
 </script>
 
 <style scoped>
@@ -22,9 +35,12 @@ import CategoryItem from './CategoryItem.vue'
   background: #fafafa;
 }
 .category-items {
-  padding: 80px 160px;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 32px;
   width: 1440px;
-  height: 352px;
+  height: 128px;
 }
 .title {
   font-family: var(--font-family);
@@ -39,5 +55,6 @@ import CategoryItem from './CategoryItem.vue'
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding-bottom: 32px;
 }
 </style>
--- src/components/CategoryItem.vue
+++ src/components/CategoryItem.vue
@@ -7,9 +7,13 @@
 
 <script setup>
 defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
   name: {
-    icon: String,
-    name: String,
+    type: String,
+    required: true,
   },
 })
 </script>
@@ -22,5 +26,27 @@ defineProps({
   height: 128px;
   min-width: 135px;
   background: #ededed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+}
+
+.icon {
+  width: 56px;
+  height: 56px;
+  object-fit: contain;
+}
+
+p {
+  font-family: var(--font-family);
+  font-style: italic;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+  text-align: center;
+  color: var(--main-black);
+  margin: 0;
 }
 </style>
