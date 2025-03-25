67c7e0b2b2bcf7e6da15c2705852ba61848066bf
VladimirKorolev <theing1986@gmail.com>
2025-03-25 21:42:20 +0300

    card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="product-card">
-    <div class="top-section">1</div>
+    <div class="top-section">
+      <img src="/Icon/Like.svg" alt="Product Image" />
+    </div>
     <div class="image-container">
-      <img src="" alt="Product Image" />
+      <img src="/Iphone14pro1.png" alt="Product Image" />
     </div>
     <div class="text-icon-container">
       <div class="text-container">
-        <p>name</p>
+        <p>Apple iPhone 14 Pro Max 128GB Deep Purple (MQ9T3RX/A)</p>
       </div>
       <div class="price-container">
-        <p>price</p>
+        <p>$900</p>
       </div>
       <button>Buy Now</button>
     </div>
@@ -20,15 +22,54 @@
 
 <style scoped>
 .product-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   border-radius: 9px;
   padding: 24px 16px;
   width: 268px;
-  height: 456px;
   min-width: 200px;
   background: #f6f6f6;
+  height: 456px;
+  justify-content: flex-start;
+  gap: 16px;
 }
 .top-section {
-  width: 236px;
-  height: 32px;
+  display: flex;
+  justify-content: flex-end;
+  width: 100%; /* Вместо 236px */
+  height: auto; /* Или минимальная высота иконки */
+}
+.text-container {
+  font-family: var(--font-family);
+  font-style: italic;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+  text-align: center;
+  color: var(--main-black);
+}
+.price-container {
+  font-family: var(--second-family);
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 100%;
+  letter-spacing: 0.03em;
+  text-align: center;
+  color: var(--main-black);
+}
+button {
+  border-radius: 8px;
+  padding: 12px 64px;
+  width: 186px;
+  height: 48px;
+  background: var(--main-black);
+  font-family: var(--font-family);
+  font-style: italic;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 171%;
+  text-align: center;
+  color: var(--main-white);
 }
 </style>
