b136672c0c1ef7a3d1fafc9ff36e904245add11c
VladimirKorolev <theing1986@gmail.com>
2025-03-29 21:37:32 +0300

    big banner

--- src/components/Banners.vue
+++ src/components/Banners.vue
@@ -1,13 +1,49 @@
 <template>
   <div class="banner">
-    <div class="big-banner">
-      <img src="" alt="" />
-      <div></div>
-      //TODO остановился на этом, необходимо закончить блок и размножить его
+    <div class="big-banner white">
+      <img src="/Group1.png" alt="часы и наушники" />
+      <div class="banner-content">
+        <b class="banner-title">Popular Products</b>
+        <p class="banner-description">
+          iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking
+          and ease of use.
+        </p>
+        <button class="banner-button">Shop Now</button>
+      </div>
     </div>
-    <div class="big-banner">2</div>
-    <div class="big-banner">3</div>
-    <div class="big-banner">4</div>
+    <div class="big-banner gray">
+      <img src="/image64.png" alt="iPad" />
+      <div class="banner-content">
+        <b class="banner-title">Ipad Pro</b>
+        <p class="banner-description">
+          iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking
+          and ease of use.
+        </p>
+        <button class="banner-button">Shop Now</button>
+      </div>
+    </div>
+    <div class="big-banner gray-light">
+      <img src="/image41.png" alt="Samsung Galaxy " />
+      <div class="banner-content">
+        <b class="banner-title">Samsung Galaxy </b>
+        <p class="banner-description">
+          iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking
+          and ease of use.
+        </p>
+        <button class="banner-button">Shop Now</button>
+      </div>
+    </div>
+    <div class="big-banner black">
+      <img src="/Macbook1.png" alt="Macbook Pro" />
+      <div class="banner-content">
+        <b class="banner-title color-white">Macbook Pro</b>
+        <p class="banner-description">
+          iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking
+          and ease of use.
+        </p>
+        <button class="banner-button">Shop Now</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,5 +53,55 @@
   height: 640px;
   display: flex;
   justify-content: space-between;
+}
+.white {
+  background: var(--main-white);
+}
+.gray {
+  background: #f9f9f9;
+}
+.gray-light {
+  background: #eaeaea;
+}
+.black {
+  background: #2c2c2c;
+}
+.big-banner {
+  display: flex;
+  flex-direction: column;
+  padding: 32px;
+  width: 360px;
+  height: 640px;
+  min-width: 280px;
+  box-sizing: border-box;
+}
+.banner-title {
+  font-family: var(--font-family);
+  font-style: italic;
+  font-weight: 400;
+  font-size: 33px;
+  line-height: 145%;
+  color: var(--main-black);
+}
+.banner-description {
+  font-family: var(--font-family);
+  font-style: italic;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 171%;
+  color: #909090;
+}
+.banner-button {
+  border: 1px solid var(--main-black);
+  border-radius: 6px;
+  padding: 16px 56px;
+  width: 188px;
+  height: 56px;
+}
+.color-white {
+  color: var(--main-white);
+}
+.banner-content {
+  margin-top: auto;
 }
 </style>
