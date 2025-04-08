c6c781d5bc7b049e59840f8073b3bd69b14e396d
VladimirKorolev <theing1986@gmail.com>
2025-04-08 22:10:16 +0300

    aside

--- src/components/FilterBlock.vue
+++ src/components/FilterBlock.vue
@@ -2,14 +2,60 @@
   <div class="filter-block">
     <div class="filter-header" @click="isOpen = !isOpen">
       <h3>{{ title }}</h3>
-      <span>{{ isOpen ? '−' : '+' }}</span>
+      <span>
+        <template v-if="isOpen">
+          <svg
+            width="12"
+            height="8"
+            viewBox="0 0 12 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6 0.295L0 6.295L1.41 7.705L6 3.125L10.59 7.705L12 6.295L6 0.295Z"
+              fill="black"
+            />
+          </svg>
+        </template>
+        <template v-else>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16.59 8.29501L12 12.875L7.41 8.29501L6 9.70501L12 15.705L18 9.70501L16.59 8.29501Z"
+              fill="black"
+            />
+          </svg>
+        </template>
+      </span>
     </div>
 
     <div v-if="isOpen" class="filter-content">
-      <input v-if="hasSearch" v-model="search" placeholder="Search..." class="filter-search" />
+      <div class="filter-search">
+        <svg
+          class="search-icon"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M20 20L16.2223 16.2156M18.3158 11.1579C18.3158 13.0563 17.5617 14.8769 16.2193 16.2193C14.8769 17.5617 13.0563 18.3158 11.1579 18.3158C9.2595 18.3158 7.43886 17.5617 6.0965 16.2193C4.75413 14.8769 4 13.0563 4 11.1579C4 9.2595 4.75413 7.43886 6.0965 6.0965C7.43886 4.75413 9.2595 4 11.1579 4C13.0563 4 14.8769 4.75413 16.2193 6.0965C17.5617 7.43886 18.3158 9.2595 18.3158 11.1579V11.1579Z"
+            stroke="#989898"
+            stroke-width="1.5"
+            stroke-linecap="round"
+          />
+        </svg>
+        <input v-if="hasSearch" v-model="search" placeholder="Search..." />
+      </div>
 
       <div class="option" v-for="option in filteredOptions" :key="option">
-        <label>
+        <label class="option-label">
           <input type="checkbox" :value="option" v-model="selectedLocal" @change="emitChange" />
           {{ option }}
         </label>
@@ -54,7 +100,6 @@ function emitChange() {
 <style scoped>
 .filter-block {
   width: 256px;
-  height: 400px;
 }
 
 .filter-header {
@@ -62,26 +107,43 @@ function emitChange() {
   justify-content: space-between;
   align-items: center;
   cursor: pointer;
-  font-weight: bold;
   margin-bottom: 8px;
+  font-family: var(--font-family);
+  font-style: italic;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 133%;
+  letter-spacing: 0.03em;
+  color: var(--main-black);
 }
 
 .filter-content {
   display: flex;
   flex-direction: column;
-  gap: 6px;
+  gap: 12px;
 }
 
 .filter-search {
-  padding: 6px;
-  margin-bottom: 6px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  border-radius: 8px;
+  padding: 8px 16px;
+  width: 256px;
+  height: 40px;
+  background: #f5f5f5;
+  opacity: 0.5;
 }
 
 .option label {
   display: flex;
   align-items: center;
   gap: 6px;
+  font-family: var(--font-family);
+  font-style: italic;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 160%;
+  color: var(--main-black);
 }
 </style>
--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -15,20 +15,49 @@ const selectedProtection = ref([])
 const selectedbuiltInMemory = ref([])
 </script>
 <template>
-  <aside class="filters">
-    <FilterBlock title="Brand" :options="brands" :hasSearch="true" v-model="selectedBrands" />
-    <FilterBlock title="Battery capacity" :options="capacities" v-model="selectedCapacities" />
-    <FilterBlock title="Screen type" :options="screenTypes" v-model="selectedScreens" />
-    <FilterBlock title="Screen diagonal" :options="screenDiagonal" v-model="selectedDiagonal" />
-    <FilterBlock title="Protection class" :options="protectionClass" v-model="selectedProtection" />
-    <FilterBlock title="Built-in memory" :options="builtInMemory" v-model="selectedbuiltInMemory" />
-  </aside>
+  <div class="products-page">
+    <aside class="filters">
+      <div class="filter-blocks">
+        <FilterBlock title="Brand" :options="brands" :hasSearch="true" v-model="selectedBrands" />
+        <FilterBlock title="Battery capacity" :options="capacities" v-model="selectedCapacities" />
+        <FilterBlock title="Screen type" :options="screenTypes" v-model="selectedScreens" />
+        <FilterBlock title="Screen diagonal" :options="screenDiagonal" v-model="selectedDiagonal" />
+        <FilterBlock
+          title="Protection class"
+          :options="protectionClass"
+          v-model="selectedProtection"
+        />
+        <FilterBlock
+          title="Built-in memory"
+          :options="builtInMemory"
+          v-model="selectedbuiltInMemory"
+        />
+      </div>
+    </aside>
+    <div class="product-list">
+      <div class="header">
+        <div class="selected-products">
+          <p>Selected Products: {{ count }}</p>
+        </div>
+        <div class="sort-by">
+          <select>
+            <option value="rating">By rating</option>
+            <option value="price-low">Price: Low to High</option>
+            <option value="price-high">Price: High to Low</option>
+          </select>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .products-page {
   display: flex;
   gap: 32px;
+  background-color: var(--main-white);
+  padding: 24px 160px 56px 160px;
 }
 
 .filters {
@@ -39,5 +68,50 @@ const selectedbuiltInMemory = ref([])
 
 .product-list {
   flex: 1;
+}
+.filter-blocks {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 8px;
+  font-family: var(--font-family);
+  font-style: italic;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 133%;
+  letter-spacing: 0.03em;
+  color: var(--main-black);
+}
+.selected-products p {
+  font-family: var(--font-family);
+  font-style: italic;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 100%;
+  letter-spacing: 0.03em;
+  color: #6c6c6c;
+}
+.sort-by select {
+  border: 0.5px solid #d4d4d4;
+  border-radius: 8px;
+  padding: 8px 16px;
+  width: 256px;
+  height: 40px;
+  min-width: 140px;
+  max-width: 256px;
+  background: var(--main-white);
+}
+option {
+  font-family: var(--second-family);
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 107%;
+  letter-spacing: -0.01em;
+  color: var(--main-black);
 }
 </style>
